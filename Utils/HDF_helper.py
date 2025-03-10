b236eafd4ba95b2ee84f66ca17a171dac48667f6
Koen J.A. Martens <32260034+kjamartens@users.noreply.github.com>
2025-03-10 15:33:26 +0100

    Added a build-in HDf5 conversion
    My metavision studio kept crashing, so this makes sense I guess

--- Utils/HDF_helper.py
+++ Utils/HDF_helper.py
@@ -22,6 +22,14 @@ def __function_metadata__():
             ],
             "help_string": "",
             "display_name": "Cut HDF5 in time"
+        },
+        "RAW_to_HDF": {
+            "required_kwargs": [
+            ],
+            "optional_kwargs": [
+            ],
+            "help_string": "",
+            "display_name": "Raw to HDF"
         }
     }
 
@@ -35,6 +43,7 @@ from PyQt5.QtWidgets import QApplication, QHBoxLayout, QVBoxLayout, QTableWidget
 from PyQt5.QtCore import Qt, QPoint, QProcess, QCoreApplication, QTimer, QFileSystemWatcher, QFile, QThread, pyqtSignal, QObject
 import sys
 import typing
+# from Utils import utils, utilsHelper
 
 def CutHDF_xy_run(loadfile,savefile,xyStretch):
     xyStretch=eval(xyStretch)
@@ -71,7 +80,53 @@ def CutHDF_time_run(loadfile,savefile,timeStretch):
         events = file.create_dataset('CD/events', data=events, compression="gzip")
     
     print('Saved')
-        
+    
+    
+def readRawTimeStretch(filepath,metaVisionPath,buffer_size = 10e7, n_batches=5e7, timeStretchMs=[0,1000]):
+    import sys
+    import logging
+    #Function to read only part of the raw file, between time stretch [0] and [1]
+    sys.path.append(metaVisionPath)
+    from metavision_core.event_io.raw_reader import RawReader
+    record_raw = RawReader(filepath,max_events=int(buffer_size))
+    #First seek to the start-time:
+    record_raw.seek_time(timeStretchMs[0]*1000)
+    #Then load the time in a single batch:
+    events = record_raw.load_delta_t(timeStretchMs[1]*1000-timeStretchMs[0]*1000)
+    record_raw.reset()
+    return events
+
+def raw_to_hdf_run(loadfile,savefile,parent,TimeMsPerStep):
+    
+    import logging
+    
+    totEvents = []
+    logging.info('Starting raw to hdf5 conversion')
+    
+    continuing = 1
+    i=0 #counter
+    while continuing:
+        try:
+            logging.info('Reading raw at time '+str(TimeMsPerStep*i))
+            events = readRawTimeStretch(loadfile,parent.globalSettings['MetaVisionPath']['value'],buffer_size = 1e7, n_batches=1e6, timeStretchMs=[TimeMsPerStep*(i),TimeMsPerStep*(i+1)])
+            if len(totEvents)==0:
+                totEvents = events
+            else:
+                totEvents = np.append(totEvents,events)
+            i=i+1
+            if len(events) == 0:
+                logging.info('Finished at time '+str(TimeMsPerStep*i))
+                continuing = 0
+        except:
+            logging.info('Finished (errored) at time '+str(TimeMsPerStep*i))
+            continuing=0
+            
+    logging.info('Starting to save')
+    #Store these events as a new hdf5:
+    with h5py.File(savefile, mode='w') as file:
+        events = file.create_dataset('CD/events', data=totEvents, compression="gzip")
+    
+    logging.info('Saved')
 
 def CutHDF_xy(parent,**kwargs):#(dataLocation,xyStretch=(-np.Inf,-np.Inf,np.Inf,np.Inf)):
     #Create a small pyqt popup window which allows you to load a file:
@@ -111,6 +166,26 @@ def CutHDF_time(parent,**kwargs):#(dataLocation,xyStretch=(-np.Inf,-np.Inf,np.In
     
     button = window.addButton("Run")
     button.clicked.connect(lambda: CutHDF_time_run(loadfileloc.text(),savefileloc.text(),timeStretchText.text()))
+    
+    window.show()
+    pass
+
+def RAW_to_HDF(parent,**kwargs):#(dataLocation,xyStretch=(-np.Inf,-np.Inf,np.Inf,np.Inf)):
+    #Create a small pyqt popup window which allows you to load a file:
+
+    try:
+        from eve_smlm.Utils import utils
+    except ImportError:
+        from Utils import utils
+
+    window = utils.SmallWindow(parent,windowTitle="RAW to HDF")
+    window.addDescription("Transform a RAW file to a HDF5 format for quicker future reading")
+    loadfileloc = window.addFileLocation()
+    savefileloc = window.addFileLocation(labelText="Save location:", textAddPrePeriod = "_hdf",textPostPeriod = "hdf5")
+    TimeMsPerStepText = window.addTextEdit(labelText="Time per step (ms):",preFilledText="120000")
+    
+    button = window.addButton("Run")
+    button.clicked.connect(lambda: raw_to_hdf_run(loadfileloc.text(),savefileloc.text(),parent,int(TimeMsPerStepText.text())))
     
     window.show()
     pass
